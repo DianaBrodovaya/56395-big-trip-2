d84d7ee486c711ee68b99ae9d6fd3ead914eac9b
DianaBrodovaya <diana.brodovaya@gmail.com>
2026-07-24 00:22:37 +0700

    Добавляет блок в шапку с наименованием маршрута, датами и общей стоимостью путешествия

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import { render, replace, remove, RenderPosition } from './framework/render.js';
 import EventModel from './model/event-model.js';
 import FilterModel from './model/filter-model.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
+import TripInfoView from './view/trip-info-view.js';
+import { sortEventDay } from './utils/sort.js';
 
 const siteHeaderElement = document.querySelector('.trip-main');
 const filtersContainer = siteHeaderElement.querySelector('.trip-controls__filters');
@@ -10,6 +13,36 @@ const newEventButtonElement = siteHeaderElement.querySelector('.trip-main__event
 
 const eventModel = new EventModel();
 const filterModel = new FilterModel();
+
+let tripInfoComponent = null;
+
+const renderTripInfo = () => {
+  const prevTripInfoComponent = tripInfoComponent;
+
+  const sortedEvents = [...eventModel.events].sort(sortEventDay);
+
+  if (sortedEvents.length === 0) {
+    if (prevTripInfoComponent) {
+      remove(prevTripInfoComponent);
+      tripInfoComponent = null;
+    }
+    return;
+  }
+
+  tripInfoComponent = new TripInfoView(sortedEvents, eventModel.destinations, eventModel.offers);
+
+  if (prevTripInfoComponent === null) {
+    render(tripInfoComponent, siteHeaderElement, RenderPosition.AFTERBEGIN);
+    return;
+  }
+
+  replace(tripInfoComponent, prevTripInfoComponent);
+  remove(prevTripInfoComponent);
+};
+
+eventModel.addObserver(() => {
+  renderTripInfo();
+});
 
 const filterPresenter = new FilterPresenter({
   filterContainer: filtersContainer,
@@ -25,6 +58,8 @@ const boardPresenter = new BoardPresenter({
   onNewEventDestroy: handleNewEventFormClose
 });
 boardPresenter.init();
+
+renderTripInfo();
 
 function handleNewEventFormClose() {
   newEventButtonElement.disabled = false;
